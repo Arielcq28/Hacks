91af054dee443b31ff38ed54c88b52e8ebc686cd
Ariel Cascante <arielcq28@hotmail.com>
2020-11-26 15:14:23 -0600

    FullPage.js React

---
+++ fullpage/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Fullpage from './components/FullPage'
+
+function App() {
+  return (
+    <div className="App">
+      <Fullpage />
+    </div>
+  );
+}
+
+export default App;
